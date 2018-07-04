0961903a5771168162174e346a4c319c302f45f3
sspitzenreiter <luthfimnabil17@gmail.com>
2018-07-04 12:00:40 +0700

    Wave 5

---
+++ DikaSukmaPradana_1174050_D4TI1B.bat
@@ -0,0 +1,45 @@
+@echo off
+:start
+echo Haaaaiiiiiii...
+echo Perkenalkan saya :
+echo Dika Sukma Pradana
+echo ------------------
+echo D4TI1B
+echo ------------------
+echo 1174050
+echo ------------------
+echo POLTEKPOS
+echo -----------------
+
+
+@echo off
+:date 
+echo %date% %time%
+
+
+@echo off
+NET CONFIG
+
+
+@echo off
+:message
+set /p ="Hitung Mundur"
+echo 3
+echo 2
+echo 1
+
+
+@echo off
+:open
+echo %windir% >>F:\tess.txt
+
+
+@echo off
+
+
+
+
+
+
+
+
